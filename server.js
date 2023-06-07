01b94858577c6f12db59231d5124d7b486cfddec
ElsdonC <elsdonc21@gmail.com>
2023-06-07 16:16:36 -0700

    update user count, emit messages

--- server.js
+++ server.js
@@ -1,17 +1,47 @@
-const express = require("express")
-const app = express()
-import { createServer } from "http";
-import { Server } from "socket.io";
-
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const bodyParser = require("body-parser")
+const app = express();
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: false }))
 const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
+io.on("connection", (socket) => {
+    const mainNS = io.of("/");
+  
+    // Emit initial count to the connecting client
+    socket.emit("initialCount", mainNS.sockets.size);
+  
+    // Broadcast count updates to all connected clients
+    const updateCount = () => {
+      io.emit("countUpdated", mainNS.sockets.size);
+    };
+    
+    updateCount();
+  
+    socket.on("disconnect", () => {
+      // Decrease count on disconnection
+      updateCount();
+    });
+
+    socket.on("sendMSG", (msg) => {
+        io.emit("msgUpdate", msg)
+    })
+});
+
+io.engine.on("connection_error", (err) => {
+    console.log(err.req);      // the request object
+    console.log(err.code);     // the error code, for example 1
+    console.log(err.message);  // the error message, for example "Session ID unknown"
+    console.log(err.context);  // some additional error context
+});
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/public/index.html")
 })
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000")
-})
+httpServer.listen(3000);
 
-httpServer.listen(3001);
+
